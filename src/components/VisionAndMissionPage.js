598c5b2eeaab0535a63e18bb20b13e8b06105f97
Vishufame <vishal.tomar@famepilot.com>
2020-07-15 21:35:54 +0530

    pages content have been added

--- src/components/VisionAndMissionPage.js
+++ src/components/VisionAndMissionPage.js
@@ -2,9 +2,31 @@ import React from 'react';
 import CommonStrip from './CommonStrip';
 
 const VisionAndMissionPage = () => {
-  return(
+  return (
     <div>
-      <CommonStrip title="Vision And Mission"/>
+      <CommonStrip title="Vision And Mission" />
+      <div className="vision-container">
+        <div className="content">
+          <section className="about-page-header">
+            <h1>Animal Welfare Society</h1>
+            <br></br>
+            <p>
+              Karma foundation’s vision & mission is to help our street animals in the best possible way utilizing the available resources optimally.</p>
+            <p>
+              We truly believe that to achieve success & to make a difference we need to utilize the existing resources & make each and every animal lover a part of the NGO.
+            </p>
+            <p>
+              We believe in utilizing the existing work force of Vets & volunteers to help the animals in need area wise & funding the treatment of such stray animals in a subsidized & case merit basis.
+            </p>
+            <p>
+              Pune city is our pilot model where we plan to use technology & our team of vets & volunteers to make a optimized & justified treatment model.
+            </p>
+            <p>
+              Once we have success in Pune, we will expand to different cities with the same model.
+            </p>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
